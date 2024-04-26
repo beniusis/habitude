8e41b79688e00fb94aa9165aa3f045cc0909d1ec
beniusis <rokasbeniu@gmail.com>
2024-04-26 18:52:03 +0300

    chore: Inter font for all of the elements

--- src/components/PageNotFound.vue
+++ src/components/PageNotFound.vue
@@ -4,11 +4,11 @@ import { RouterLink } from 'vue-router';
 
 <template>
   <main class="flex flex-col items-center justify-center w-full h-screen select-none">
-    <h1 class="font-extrabold tracking-widest text-text text-9xl">404</h1>
+    <h1 class="font-black tracking-widest text-text text-9xl">404</h1>
     <div class="absolute px-2 text-sm rounded bg-primary rotate-12">Page Not Found</div>
     <RouterLink
       to="/"
-      class="px-8 py-3 mt-4 text-sm font-medium duration-500 ease-out rounded-md bg-primary hover:bg-secondary"
+      class="px-8 py-3 mt-4 text-sm font-semibold duration-500 ease-out rounded-md bg-primary hover:bg-secondary"
       >Go Home</RouterLink
     >
   </main>
