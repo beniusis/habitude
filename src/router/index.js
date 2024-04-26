086b4ba9b9499d76e6ea8b19846f8f823e61e97e
beniusis <rokasbeniu@gmail.com>
2024-04-26 18:45:39 +0300

    feat: finished the router configuration

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,28 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/day/:date',
+      name: 'day',
+      component: () => import('@/views/DayView.vue'),
+      beforeEnter: (to, _, next) => {
+        const regex = /^\d{4}-\d{2}-\d{2}$/;
+
+        if (regex.test(to.params.date)) {
+          next();
+        } else {
+          next({
+            name: 'not-found',
+            params: { pathMatch: to.path.substring(1).split('/') }
+          });
+        }
+      }
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'not-found',
+      component: () => import('@/components/PageNotFound.vue')
     }
   ]
 });
