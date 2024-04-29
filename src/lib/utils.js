110b0b54fea7e2f63a3d76c6e7424c23d9b79e25
beniusis <rokasbeniu@gmail.com>
2024-04-29 19:49:19 +0300

    feat: created a function to get the dates of the current week

---
+++ src/lib/utils.js
@@ -0,0 +1,19 @@
+/* eslint-disable import/prefer-default-export */
+
+/**
+ * Get the dates of the current week.
+ * @returns {Array<string>} Array of dates in ISO format (YYYY-MM-DD).
+ */
+export const getCurrentWeekDates = () => {
+  const dates = [];
+  const currentDate = new Date();
+
+  for (let i = 0; i <= 6; i += 1) {
+    // The first day of the week is Monday
+    const first = currentDate.getDate() - currentDate.getDay() + 1 + i;
+    const day = new Date(currentDate.setDate(first)).toISOString().slice(0, 10);
+    dates.push(day);
+  }
+
+  return dates;
+};
