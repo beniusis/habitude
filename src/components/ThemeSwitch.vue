55658b856f76ae77f7df48a4dd352e55f7a5e8c0
beniusis <rokasbeniu@gmail.com>
2024-04-30 21:16:24 +0300

    feat: created a theme switcher component

---
+++ src/components/ThemeSwitch.vue
@@ -0,0 +1,13 @@
+<template>
+  <button class="text-2xl" type="button" @click="switchTheme()">
+    {{ isDark ? '🌚' : '🌞' }}
+  </button>
+</template>
+
+<script setup>
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { useDark, useToggle } from '@vueuse/core';
+
+const isDark = useDark();
+const switchTheme = useToggle(isDark);
+</script>
