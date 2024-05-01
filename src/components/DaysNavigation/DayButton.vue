cd8065d42c69fe0f5191a0312e934f711060b2b6
beniusis <rokasbeniu@gmail.com>
2024-05-01 19:06:22 +0300

    feat: created a DaysNavigation component
    Additionally created a separate component for each day as a button.

---
+++ src/components/DaysNavigation/DayButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <button
+    :value="date"
+    @click="$router.push(`/day/${date}`)"
+    type="button"
+    class="flex items-center justify-center w-10 h-10 text-xs font-medium rounded-full bg-primary text-text hover:bg-accent"
+  >
+    {{ toWeekDay(date) }}
+  </button>
+</template>
+
+<script setup>
+import { toWeekDay } from '@/lib/utils';
+
+defineProps({
+  date: {
+    type: String,
+    default: undefined
+  }
+});
+</script>
---
+++ src/components/DaysNavigation/DaysNavigation.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex flex-row flex-wrap items-center justify-center gap-2 pt-10">
+    <DayButton v-for="date in dates" :date="date" :key="date" />
+  </div>
+</template>
+
+<script setup>
+import DayButton from './DayButton.vue';
+
+defineProps({
+  dates: {
+    type: Array,
+    default: () => []
+  }
+});
+</script>
