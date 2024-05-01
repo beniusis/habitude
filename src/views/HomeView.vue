25c2e50cec063f8219c670205b5067fc8eb30220
beniusis <rokasbeniu@gmail.com>
2024-05-01 19:14:16 +0300

    feat: main view layout

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,10 @@
 <template>
-  <main></main>
+  <main class="flex flex-col items-center justify-center gap-10 px-5">
+    <DaysNavigation :dates="getCurrentWeekDates()" />
+  </main>
 </template>
+
+<script setup>
+import { getCurrentWeekDates } from '@/lib/utils';
+import DaysNavigation from '@/components/DaysNavigation/DaysNavigation.vue';
+</script>
