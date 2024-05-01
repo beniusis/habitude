5a5b36be2d650ba857beb67813ddf0dbe9da8cff
beniusis <rokasbeniu@gmail.com>
2024-05-01 15:53:16 +0300

    fix: fixed switching between light/dark modes

--- src/components/ThemeSwitch.vue
+++ src/components/ThemeSwitch.vue
@@ -1,11 +1,10 @@
 <template>
   <button class="text-2xl" type="button" @click="switchTheme()">
-    {{ isDark ? '🌚' : '🌞' }}
+    {{ isDark ? '🌞' : '🌚' }}
   </button>
 </template>
 
 <script setup>
-// eslint-disable-next-line import/no-extraneous-dependencies
 import { useDark, useToggle } from '@vueuse/core';
 
 const isDark = useDark();
