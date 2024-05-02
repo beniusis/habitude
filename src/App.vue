090017bc01adcd740b2f967a71cc15d1fa8a6132
beniusis <rokasbeniu@gmail.com>
2024-05-02 17:19:11 +0300

    refactor: created a component for the header

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,9 @@
 <template>
-  <div class="flex flex-col items-center justify-center gap-5 mt-10">
-    <ThemeSwitch />
-    <h1
-      class="text-5xl font-black tracking-widest text-center duration-300 ease-out select-none text-text hover:rotate-1 hover:scale-95 sm:text-6xl"
-    >
-      <a href="/" @click.prevent="$router.push('/')">habitude</a>
-    </h1>
-  </div>
-
+  <AppHeader />
   <RouterView />
 </template>
 
 <script setup>
 import { RouterView } from 'vue-router';
-import ThemeSwitch from './components/ThemeSwitch.vue';
+import AppHeader from './components/AppHeader.vue';
 </script>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,14 @@
+<template>
+  <header class="flex flex-col items-center justify-center gap-5 mt-10">
+    <ThemeSwitch />
+    <h1
+      class="text-5xl font-black tracking-widest text-center duration-300 ease-out select-none text-text hover:rotate-1 hover:scale-95 sm:text-6xl"
+    >
+      <a href="/" @click.prevent="$router.push('/')">habitude</a>
+    </h1>
+  </header>
+</template>
+
+<script setup>
+import ThemeSwitch from './ThemeSwitch.vue';
+</script>
