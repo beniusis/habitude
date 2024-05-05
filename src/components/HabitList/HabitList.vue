0011b0a1e7b3662219935891357d45568eb45014
beniusis <rokasbeniu@gmail.com>
2024-05-05 17:52:44 +0300

    impr: styled the HabitList component

--- src/components/HabitList/HabitList.vue
+++ src/components/HabitList/HabitList.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="flex flex-col items-center justify-center gap-5">
-    <div v-if="habits.length > 0"></div>
+    <div v-if="habits.length > 0" class="flex flex-col items-center justify-center gap-4">
+      <h2 class="text-2xl font-bold">Habit List Items</h2>
+      <div class="flex flex-col items-center justify-center gap-2">
+        <HabitItem v-for="habit in habits" :data="habit" :key="habit.id" />
+      </div>
+    </div>
     <div v-else class="text-3xl text-primary">Currently there are no habits in the list!</div>
   </div>
 </template>
 
 <script setup>
+import HabitItem from './HabitItem.vue';
+
 defineProps({
   habits: {
     type: Array,
