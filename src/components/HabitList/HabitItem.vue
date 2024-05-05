f3e9fa1188a425df4ae2e474903b828275d6181a
beniusis <rokasbeniu@gmail.com>
2024-05-05 17:52:08 +0300

    impr: first styling version for HabitItem component

--- src/components/HabitList/HabitItem.vue
+++ src/components/HabitList/HabitItem.vue
@@ -1,8 +1,21 @@
 <template>
-  <div></div>
+  <div class="flex items-center justify-center gap-2">
+    <div class="text-center font-medium capitalize">{{ data.name }}</div>
+    <button
+      class="rounded-md bg-red-500 p-1 text-xs text-text"
+      type="button"
+      @click="habits.remove(data.id)"
+    >
+      del
+    </button>
+  </div>
 </template>
 
 <script setup>
+import useHabitsStore from '@/stores/habits';
+
+const habits = useHabitsStore();
+
 defineProps({
   data: {
     type: Object,
