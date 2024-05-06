87c60392fbe5f5827c0b86b6c9f39e8800014a41
beniusis <rokasbeniu@gmail.com>
2024-05-06 14:53:40 +0300

    feat: created a reusable PrimaryButton component

---
+++ src/components/PrimaryButton.vue
@@ -0,0 +1,8 @@
+<template>
+  <button
+    class="rounded-md bg-primary px-4 py-2 text-sm text-text duration-300 ease-out hover:bg-accent active:scale-95 disabled:hover:bg-primary disabled:active:scale-100"
+    type="button"
+  >
+    <slot></slot>
+  </button>
+</template>
