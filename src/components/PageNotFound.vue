ac9da8c10934c91b3690a84a3cad1286cf6f8575
beniusis <rokasbeniu@gmail.com>
2024-05-06 14:52:24 +0300

    impr: fixed the styling

--- src/components/PageNotFound.vue
+++ src/components/PageNotFound.vue
@@ -3,12 +3,14 @@ import { RouterLink } from 'vue-router';
 </script>
 
 <template>
-  <main class="flex flex-col items-center justify-center w-full h-screen select-none">
-    <h1 class="font-black tracking-widest text-text text-9xl">404</h1>
-    <div class="absolute px-2 text-sm rounded bg-primary rotate-12">Page Not Found</div>
+  <main
+    class="absolute top-1/2 flex w-full -translate-y-1/2 select-none flex-col items-center justify-center"
+  >
+    <h1 class="text-9xl font-black tracking-widest text-text">404</h1>
+    <div class="absolute rotate-12 rounded bg-primary px-2 text-sm">Page Not Found</div>
     <RouterLink
       to="/"
-      class="px-8 py-3 mt-4 text-sm font-semibold duration-500 ease-out rounded-md bg-primary hover:bg-secondary"
+      class="mt-4 rounded-md bg-primary px-8 py-3 text-sm font-semibold duration-500 ease-out hover:bg-secondary"
       >Go Home</RouterLink
     >
   </main>
