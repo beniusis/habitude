9c0b2b1f86ac0bf5e157b7cbe8020b5cc062501f
beniusis <rokasbeniu@gmail.com>
2024-05-06 15:25:57 +0300

    impr: added DatePicker before buttons for the week days

--- src/components/DaysNavigation/DaysNavigation.vue
+++ src/components/DaysNavigation/DaysNavigation.vue
@@ -1,10 +1,14 @@
 <template>
-  <div class="flex flex-row flex-wrap items-center justify-center gap-2">
-    <DayButton v-for="date in dates" :date="date" :key="date" />
+  <div class="flex flex-col items-center justify-center gap-10">
+    <DatePicker />
+    <div class="flex flex-row flex-wrap items-center justify-center gap-2">
+      <DayButton v-for="date in dates" :date="date" :key="date" />
+    </div>
   </div>
 </template>
 
 <script setup>
+import DatePicker from '../DatePicker.vue';
 import DayButton from './DayButton.vue';
 
 defineProps({
