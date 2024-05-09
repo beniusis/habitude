6deef680aa190ab7b116003e25bd68d15c8bd235
beniusis <rokasbeniu@gmail.com>
2024-05-09 14:13:03 +0300

    impr: showing a month and a day above the buttons

--- src/components/DaysNavigation/DayButton.vue
+++ src/components/DaysNavigation/DayButton.vue
@@ -1,17 +1,22 @@
 <template>
-  <button
-    class="flex items-center justify-center w-10 h-10 text-xs font-semibold duration-300 ease-in-out rounded-full bg-primary text-text hover:scale-105 hover:bg-accent disabled:hover:scale-100 disabled:hover:bg-primary"
-    type="button"
-    :value="date"
-    :disabled="isDayDisabled(date)"
-    @click="$router.push(`/day/${date}`)"
-  >
-    {{ toWeekDay(date) }}
-  </button>
+  <div class="flex flex-col items-center justify-center gap-1">
+    <div class="select-none text-xs">
+      {{ getMonthAndDay(date) }}
+    </div>
+    <button
+      class="flex h-10 w-10 items-center justify-center rounded-full bg-primary text-xs font-semibold text-text duration-300 ease-in-out hover:scale-105 hover:bg-accent disabled:hover:scale-100 disabled:hover:bg-primary"
+      type="button"
+      :value="date"
+      :disabled="isDayDisabled(date)"
+      @click="$router.push(`/day/${date}`)"
+    >
+      {{ toWeekDay(date) }}
+    </button>
+  </div>
 </template>
 
 <script setup>
-import { isDayDisabled, toWeekDay } from '@/lib/utils';
+import { getMonthAndDay, isDayDisabled, toWeekDay } from '@/lib/utils';
 
 defineProps({
   date: {
