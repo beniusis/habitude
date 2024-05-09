0654940ac229dbfd0e48f769a6db41633be2304c
beniusis <rokasbeniu@gmail.com>
2024-05-09 13:27:39 +0300

    chore: changed the font size according to the window size

--- src/components/PrimaryButton.vue
+++ src/components/PrimaryButton.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="rounded-md bg-primary px-4 py-2 text-sm text-text duration-300 ease-out hover:bg-accent active:scale-95 disabled:hover:bg-primary disabled:active:scale-100"
+    class="rounded-md bg-primary px-4 py-2 text-xs text-text duration-300 ease-out hover:bg-accent active:scale-95 disabled:hover:bg-primary disabled:active:scale-100 sm:text-sm"
     type="button"
   >
     <slot></slot>
