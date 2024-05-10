36bdb7ecc9d7d18ad30eaca7874e7d092523ed0f
beniusis <rokasbeniu@gmail.com>
2024-05-10 18:27:28 +0300

    impr: added edit, update, stop methods

--- src/stores/habits.js
+++ src/stores/habits.js
@@ -1,3 +1,4 @@
+import { formatDate } from '@/lib/utils';
 import { useLocalStorage } from '@vueuse/core';
 import { defineStore } from 'pinia';
 import { ref, watch } from 'vue';
@@ -17,7 +18,8 @@ const useHabitsStore = defineStore('habits', () => {
     list.value.push({
       id: list.value.length > 0 ? list.value[list.value.length - 1].id + 1 : 1,
       name: habitName,
-      addedAt: new Date().toISOString().slice(0, 10)
+      completedAt: {},
+      stoppedAt: null
     });
   };
 
@@ -25,11 +27,31 @@ const useHabitsStore = defineStore('habits', () => {
     list.value = list.value.filter((h) => h.id !== id);
   };
 
+  const edit = (id, name) => {
+    const habit = list.value.find((h) => h.id === id);
+    habit.name = name;
+  };
+
+  const update = (id, date, status) => {
+    const habit = list.value.find((h) => h.id === id);
+
+    if (status === false) {
+      delete habit.completedAt[date];
+    } else {
+      habit.completedAt[date] = status;
+    }
+  };
+
+  const stop = (id, date) => {
+    const habit = list.value.find((h) => h.id === id);
+    habit.stoppedAt = formatDate(date);
+  };
+
   watch(list, () => {
     habitsStorage.value = list.value;
   });
 
-  return { list, add, remove };
+  return { list, add, remove, edit, update, stop };
 });
 
 export default useHabitsStore;
