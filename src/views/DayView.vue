1d4c1486f66ed6eced83565652d159928031ffbe
beniusis <rokasbeniu@gmail.com>
2024-05-11 11:18:40 +0300

    impr: fixed the problem with showing tracked habits if it's value stoppedAt is null

--- src/views/DayView.vue
+++ src/views/DayView.vue
@@ -12,20 +12,23 @@ const habits = useHabitsStore();
 const { list } = storeToRefs(habits);
 
 const habitList = computed(() => {
-  return list.value.filter((h) => new Date(h.stoppedAt).getTime() >= new Date(date).getTime());
+  return list.value.filter((h) => {
+    if (h.stoppedAt === null) return list.value;
+    return new Date(h.stoppedAt).getTime() >= new Date(date).getTime();
+  });
 });
 </script>
 
 <template>
   <h2
-    class="mt-10 text-center text-2xl font-semibold text-text transition-all duration-300 ease-out sm:text-3xl"
+    class="mt-10 text-2xl font-semibold text-center transition-all duration-300 ease-out text-text sm:text-3xl"
   >
     Tracked Habits
   </h2>
-  <div class="text-center text-sm font-medium text-text sm:text-base">{{ date }}</div>
+  <div class="text-sm font-medium text-center text-text sm:text-base">{{ date }}</div>
   <div
     v-if="habitList.length === 0"
-    class="mt-4 text-center text-base font-medium text-text sm:text-xl"
+    class="mt-4 text-base font-medium text-center text-text sm:text-xl"
   >
     There are no habits being tracked
   </div>
@@ -33,11 +36,11 @@ const habitList = computed(() => {
     v-else
     v-for="habit in habitList"
     :key="habit.id"
-    class="mt-4 flex flex-col items-center justify-center gap-2 sm:flex-row"
+    class="flex flex-col items-center justify-center gap-2 mt-4 sm:flex-row"
   >
     <label class="sr-only" for="isCompleted" name="isCompleted">Is habit completed</label>
     <input
-      class="h-4 w-4 bg-background accent-accent"
+      class="w-4 h-4 bg-background accent-accent"
       type="checkbox"
       id="isCompleted"
       :checked="habit.completedAt[date]"
