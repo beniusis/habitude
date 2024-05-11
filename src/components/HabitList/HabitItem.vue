26ef938b814a28f76dc1acfae502f07f45576ca8
beniusis <rokasbeniu@gmail.com>
2024-05-11 12:03:22 +0300

    refactor: using the new modal component

--- src/components/HabitList/HabitItem.vue
+++ src/components/HabitList/HabitItem.vue
@@ -1,38 +1,11 @@
-<template>
-  <div class="flex flex-col items-center justify-center gap-2 sm:flex-row">
-    <div
-      class="text-sm font-medium text-center capitalize text-text sm:text-base"
-      :class="{ 'line-through': isCompleted }"
-    >
-      {{ data.name }}
-    </div>
-    <div class="flex flex-row items-center justify-center gap-1">
-      <button
-        class="p-1 text-xs bg-red-500 rounded-md text-text"
-        type="button"
-        @click="habits.remove(data.id)"
-      >
-        del
-      </button>
-      <button
-        v-if="isStoppable"
-        class="p-1 text-xs bg-orange-500 rounded-md text-text"
-        type="button"
-        @click="habits.stop(data.id, $route.params.date)"
-      >
-        stop
-      </button>
-      <button v-if="isEditable" class="p-1 text-xs bg-green-500 rounded-md text-text" type="button">
-        edit
-      </button>
-    </div>
-  </div>
-</template>
-
 <script setup>
 import useHabitsStore from '@/stores/habits';
+import { ref } from 'vue';
+import HabitModal from '../HabitModal.vue';
 
 const habits = useHabitsStore();
+
+const openModal = ref(false);
 
 defineProps({
   data: {
@@ -53,3 +26,40 @@ defineProps({
   }
 });
 </script>
+
+<template>
+  <div class="flex flex-col items-center justify-center gap-2 sm:flex-row">
+    <div
+      class="text-center text-sm font-medium capitalize text-text sm:text-base"
+      :class="{ 'line-through': isCompleted }"
+    >
+      {{ data.name }}
+    </div>
+    <div class="flex flex-row items-center justify-center gap-1">
+      <button
+        class="rounded-md bg-red-500 p-1 text-xs text-text"
+        type="button"
+        @click="habits.remove(data.id)"
+      >
+        del
+      </button>
+      <button
+        v-if="isStoppable"
+        class="rounded-md bg-orange-500 p-1 text-xs text-text"
+        type="button"
+        @click="habits.stop(data.id, $route.params.date)"
+      >
+        stop
+      </button>
+      <button
+        v-if="isEditable"
+        class="rounded-md bg-green-500 p-1 text-xs text-text"
+        type="button"
+        @click="openModal = true"
+      >
+        edit
+      </button>
+    </div>
+  </div>
+  <HabitModal :data="data" :is-open="openModal" :type="'edit'" @close-edit="openModal = false" />
+</template>
--- src/components/HabitList/HabitList.vue
+++ src/components/HabitList/HabitList.vue
@@ -1,21 +1,10 @@
-<template>
-  <div class="flex flex-col items-center justify-center gap-5">
-    <div v-if="habits.length > 0" class="flex flex-col items-center justify-center gap-4">
-      <h2 class="text-lg font-bold text-center duration-300 ease-in-out text-text sm:text-2xl">
-        Habit List - Daily Tracked Habits
-      </h2>
-      <div class="flex flex-col items-center justify-center gap-2">
-        <HabitItem v-for="habit in habits" :data="habit" :key="habit.id" />
-      </div>
-    </div>
-    <div v-else class="text-xl text-center duration-300 ease-in-out text-primary sm:text-3xl">
-      Currently there are no habits in the list!
-    </div>
-  </div>
-</template>
-
 <script setup>
+import { ref } from 'vue';
 import HabitItem from './HabitItem.vue';
+import PrimaryButton from '../PrimaryButton.vue';
+import HabitModal from '../HabitModal.vue';
+
+const openModal = ref(false);
 
 defineProps({
   habits: {
@@ -24,3 +13,21 @@ defineProps({
   }
 });
 </script>
+
+<template>
+  <div class="flex flex-col items-center justify-center gap-5">
+    <div v-if="habits.length > 0" class="flex flex-col items-center justify-center gap-4">
+      <h2 class="text-center text-lg font-bold text-text duration-300 ease-in-out sm:text-2xl">
+        Habit List - Daily Tracked Habits
+      </h2>
+      <div class="flex flex-col items-center justify-center gap-2">
+        <HabitItem v-for="habit in habits" :data="habit" :key="habit.id" />
+      </div>
+    </div>
+    <div v-else class="text-center text-xl text-primary duration-300 ease-in-out sm:text-3xl">
+      Currently there are no habits in the list!
+    </div>
+  </div>
+  <PrimaryButton @click="openModal = true">Add new habit</PrimaryButton>
+  <HabitModal :is-open="openModal" @close-add="openModal = false" />
+</template>
