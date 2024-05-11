a0b592f676e9def1bf81421ef2ed5992ef5b5175
beniusis <rokasbeniu@gmail.com>
2024-05-11 12:08:45 +0300

    docs: documentation of the habits store methods

--- src/stores/habits.js
+++ src/stores/habits.js
@@ -5,6 +5,11 @@ import { ref, watch } from 'vue';
 
 const habitsStorage = useLocalStorage('habits', []);
 
+/**
+ * Checks if a habit with the provided name already exists in the habit list.
+ * @param {string} name
+ * @returns {Boolean} True - if the habit with the provided name already exists in the habit list. False - if it does not exist.
+ */
 const exists = (name) => {
   return habitsStorage.value.some((h) => h.name === name);
 };
@@ -12,6 +17,11 @@ const exists = (name) => {
 const useHabitsStore = defineStore('habits', () => {
   const list = ref(habitsStorage.value);
 
+  /**
+   * Adds a new habit to the habit list.
+   * @param {string} habitName
+   * @returns If the habit with the provided name already exists in the habit list.
+   */
   const add = (habitName) => {
     if (exists(habitName)) return;
 
@@ -23,15 +33,33 @@ const useHabitsStore = defineStore('habits', () => {
     });
   };
 
+  /**
+   * Removes a habit from the habit list.
+   * @param {number} id
+   */
   const remove = (id) => {
     list.value = list.value.filter((h) => h.id !== id);
   };
 
+  /**
+   * Changes the existing habit's name.
+   * @param {number} id
+   * @param {string} name
+   * @returns If the habit with the provided name already exists in the habit list.
+   */
   const edit = (id, name) => {
+    if (exists(name)) return;
+
     const habit = list.value.find((h) => h.id === id);
     habit.name = name;
   };
 
+  /**
+   * Updates the completion status of a habit on a specific date.
+   * @param {number} id
+   * @param {string} date
+   * @param {boolean} status
+   */
   const update = (id, date, status) => {
     const habit = list.value.find((h) => h.id === id);
 
@@ -42,6 +70,11 @@ const useHabitsStore = defineStore('habits', () => {
     }
   };
 
+  /**
+   * Stops a habit from being tracked from the specific date going forward.
+   * @param {number} id
+   * @param {string} date
+   */
   const stop = (id, date) => {
     const habit = list.value.find((h) => h.id === id);
     habit.stoppedAt = formatDate(date);
