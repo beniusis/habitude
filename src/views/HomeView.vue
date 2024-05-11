967a797aefe40435e1f4aac8bd041e1dd333b691
beniusis <rokasbeniu@gmail.com>
2024-05-11 12:03:57 +0300

    style: moved the script to the top

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,17 @@
-<template>
-  <main class="mt-10 flex flex-col items-center justify-center gap-10">
-    <DaysNavigation :dates="getLastSevenDaysDates()" />
-    <HabitList :habits="list" />
-    <AddHabitModal />
-  </main>
-</template>
-
 <script setup>
 import { getLastSevenDaysDates } from '@/lib/utils';
 import useHabitsStore from '@/stores/habits';
 import { storeToRefs } from 'pinia';
 import DaysNavigation from '@/components/DaysNavigation/DaysNavigation.vue';
 import HabitList from '@/components/HabitList/HabitList.vue';
-import AddHabitModal from '@/components/AddHabitModal.vue';
 
 const habits = useHabitsStore();
 const { list } = storeToRefs(habits);
 </script>
+
+<template>
+  <main class="mt-10 flex flex-col items-center justify-center gap-10">
+    <DaysNavigation :dates="getLastSevenDaysDates()" />
+    <HabitList :habits="list" />
+  </main>
+</template>
